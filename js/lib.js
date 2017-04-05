4047e00f7078d81d18c00d355852dc88ba306b7f
kornienko.andriy <kornienko.andriy@gmail.com>
2017-04-05 15:50:15 +0300

    befor clear and hosting

--- js/lib.js
+++ js/lib.js
@@ -1,22 +1,21 @@
-(function($) {
-$(function() {
-  $('ul.block_30').on('click', 'li:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div.tabs').find('div.block_24').removeClass('active').eq($(this).index()).addClass('active');
-  });
-});
-})(jQuery);
+$(document).ready(function () {
 
-// $(function () {
-//     $('.modal_temp').leanModal({top: "20%" , closeButton: ".modal_close" });
-// });
+    (function($) {
+    $(function() {
+      $('ul.block_30').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.block_24').removeClass('active').eq($(this).index()).addClass('active');
+      });
+    });
+    })(jQuery);
 
+    $('.modal_close').on('click', function() {
+        $.modal.close();
+        var link = $(this).attr('href');
+        location.href = link;
+        return false;
+    });
 
-$('.modal_close').on('click', function() {
-    $.modal.close();
-    var link = $(this).attr('href');
-    location.href = link;
-    return false;
 });
 
